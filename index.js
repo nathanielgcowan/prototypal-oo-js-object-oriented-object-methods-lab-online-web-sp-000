961782a0dd8e5feb2548cc6e628fda8a445eeaff
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-23 12:37:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,26 @@ function BoardMember(name, homeState,training) {
   this.name = name;
   this.homeState = homeState;
   this.training = training;
-  this.veto = function(){
-    console.log(`No, I must disagree`)
-  }
 }
+
+BoardMember.prototype.veto = function() {
+  return "No, I must disagree"
+};
+
+BoardMember.prototype.approve = function() {
+  return "You can do that!"
+};
+
+BoardMember.prototype.doCharity = function() {
+  return "I like to help people."
+};
+
+BoardMember.prototype.releasePressStatement = function() {
+  return "You will see great things from Scuber."
+};
+
+BoardMember.prototype.sayHi = function() {
+  return "Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}."
+};
+
+let BoardMember = new BoardMember
